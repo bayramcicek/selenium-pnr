3fff9234a9f04b537a7e2f60d642b39fd829087b
bayramcicek <mail@bayramcicek.com.tr>
2019-09-04 13:28:41 +0300

    initial commit

---
+++ pnr_package/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3.6
+# created by cicek on 4.09.2019 12:00
+
+from setuptools import setup
+
+setup(
+    name='selenium-pnr',
+    version='1.0',
+    scripts=['selenium-pnr'],
+    description='selenium pnr (intern-project)',
+    url=''
+)
